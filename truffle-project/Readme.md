ba4e811437ae5dcafd5bb71bdac32465e736bcf7
tkeber <thilo.keber@metafinanz.de>
2016-08-18 13:21:10 +0200

    added contracts diagram

---
+++ truffle-project/Readme.md
@@ -0,0 +1,8 @@
+## Documentation
+
+### Diagrams
+
+use graphviz to render diagrams in `../dot`, e.g.
+
+    dot -Tpng dot/contracts.dot > doc/contracts.png
+    
--- truffle-project/scripts/init_blockchain.js
+++ truffle-project/scripts/init_blockchain.js
@@ -1,10 +1,12 @@
-module.exports = function(callback) {
+module.exports = (callback) => {
+    var accounts = web3.eth.accounts;
+    var admin = accounts[0];
+    var cp1 = accounts[1];
     var registry = Registry.deployed();
 
     // add consortium member
     registry.getContractAddress.call("consortium").then((cons) => {
-        
+        consortium = Consortium.at(cons);
+        consortium.addPartner("Consortium Partner 1", cp1);
     });
-
-    // set registry address in registry aware contracts
 };
