53f91d5f2f7533926b2e96a6bbf4acbfa9f92aba
tkeber <thilo.keber@metafinanz.de>
2016-08-22 18:06:45 +0200

    web app: move to angular / bootstrap (WIP)

--- truffle-project/app/javascripts/app.js
+++ truffle-project/app/javascripts/app.js
@@ -40,23 +40,23 @@ function buildProxy() {
       setStatus("Error building proxy; see log.");
     });
   });
-}
+}  web3.eth.getAccounts(function(err, accs) {
+  if (err != null) {
+    alert("There was an error fetching your accounts.");
+    return;
+  }
+
+  if (accs.length == 0) {
+    alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+    return;
+  }
+
+  accounts = accs;
+  account = accounts[0];
+
+  // watchProxiesBuilt();
+});
 
 window.onload = function() {
-  web3.eth.getAccounts(function(err, accs) {
-    if (err != null) {
-      alert("There was an error fetching your accounts.");
-      return;
-    }
 
-    if (accs.length == 0) {
-      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-      return;
-    }
-
-    accounts = accs;
-    account = accounts[0];
-
-    watchProxiesBuilt();
-  });
 };
--- truffle-project/truffle.js
+++ truffle-project/truffle.js
@@ -1,13 +1,25 @@
 module.exports = {
   build: {
     "index.html": "index.html",
+    "pages/": "pages/",
     "app.js": [
-      "javascripts/app.js"
+      "javascripts/angular.min.js",
+      "javascripts/angular-route.min.js",
+      "javascripts/angular-sanitize.min.js",
+      "javascripts/jquery-3.1.0.slim.min.js",
+      "external/bootstrap-3.3.7-dist/js/bootstrap.min.js",
+      "external/ngToast/ngToast.min.js",
+      "javascripts/app.js",
+      "javascripts/controller/script.js"
     ],
     "app.css": [
-      "stylesheets/app.css"
+      "external/bootstrap-3.3.7-dist/css/bootstrap.min.css",
+      "external/font-awesome-4.6.3/css/font-awesome.min.css",
+      "external/ngToast/ngToast-animations.min.css",
+      "external/ngToast/ngToast.min.css"
     ],
-    "images/": "images/"
+    "images/": "images/",
+    "fonts/": "external/font-awesome-4.6.3/fonts/"
   },
   rpc: {
     host: "localhost",
